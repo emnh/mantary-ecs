16891e3e2fc4ffd629d4bd63a1aca58f61166111
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 23:24:52 +0200

    add function_createPlatform.js

--- ecs/src/functions/function_createPlatform.js
+++ ecs/src/functions/function_createPlatform.js
@@ -1,9 +1,3 @@
 function createPlatform(x, y, width, height, color) {
-  return {
-    x: x,
-    y: y,
-    width: width,
-    height: height,
-    color: color
-  };
+  return { x: x, y: y, width: width, height: height, color: color };
 }
