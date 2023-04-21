b0930e438f5ca53b8f30e999b33c9286547360d9
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 23:58:56 +0200

    add function_uploadFileToServer.js

--- ecs/src/functions/function_uploadFileToServer.js
+++ ecs/src/functions/function_uploadFileToServer.js
@@ -1,7 +1,11 @@
 function uploadFileToServer(filename, content) {
+  console.log('uploadFileToServer');
   return new Promise((resolve, reject) => {
     const url = 'http://localhost:5000/upload';
-    const data = { filename: filename, content: content };
+    const data = {
+      filename: filename,
+      content: content
+    };
     const options = {
       method: 'POST',
       headers: {
@@ -9,15 +13,12 @@ function uploadFileToServer(filename, content) {
       },
       body: JSON.stringify(data)
     };
-    fetch(url, options)
-      .then(response => response.text())
-      .then(result => {
-        console.log(filename, result);
-        resolve(result);
-      })
-      .catch(error => {
-        console.error(filename, error);
-        reject(error);
-      });
+    fetch(url, options).then(response => response.text()).then(result => {
+      console.log(filename, result);
+      resolve(result);
+    }).catch(error => {
+      console.error(filename, error);
+      reject(error);
+    });
   });
 }
