50c27536ff073fb65f4704d2d68423050e440735
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 23:20:41 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,30 +1,18 @@
 function main() {
-  // Set up the game canvas
-  const canvas = createCanvas(800, 600);
-  document.body.appendChild(canvas);
-  const ctx = canvas.getContext("2d");
+  // Set the background color to red
+  document.body.style.backgroundColor = "red";
 
-  // Create the game objects
-  const player = createPlayer(50, 50, "blue");
-  const platforms = [    createPlatform(0, 550, canvas.width, 50, "grey"),    createPlatform(50, 400, 200, 50, "brown"),    createPlatform(500, 300, 200, 50, "brown"),    createPlatform(300, 200, 200, 50, "brown"),    createPlatform(50, 100, 200, 50, "brown")  ];
+  // Create the header element
+  const header = createHeader("Welcome to my cool webpage!");
 
-  // Set up the game loop
-  const keys = {};
-  const update = () => {
-    handleInput(player, keys);
-    updatePlayer(player, platforms, canvas);
-    drawGame(ctx, platforms, player);
-    requestAnimationFrame(update);
-  };
+  // Create the main content area
+  const main = createMain("<p>This is the main content area of my cool webpage.</p><p>Here's a cool image:</p><img src='https://picsum.photos/400' alt='A random image from Lorem Picsum'>");
 
-  // Set up keyboard event listeners
-  document.addEventListener("keydown", event => {
-    keys[event.code] = true;
-  });
-  document.addEventListener("keyup", event => {
-    keys[event.code] = false;
-  });
+  // Create the footer element
+  const footer = createFooter("&copy; 2023 My Cool Webpage. All rights reserved.");
 
-  // Start the game loop
-  requestAnimationFrame(update);
+  // Add the header, main content area, and footer to the page
+  addElementToBody(header);
+  addElementToBody(main);
+  addElementToBody(footer);
 }
