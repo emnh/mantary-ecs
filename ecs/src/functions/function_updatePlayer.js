e74700bc419692485e5d7f64f13a799de23717f7
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 00:00:42 +0200

    add function_updatePlayer.js

--- ecs/src/functions/function_updatePlayer.js
+++ ecs/src/functions/function_updatePlayer.js
@@ -1,9 +1,1 @@
-function main() {
-  console.log('main');
-}
-function createCanvas(width, height) {}
-function createPlayer(x, y, color) {}
-function createPlatform(x, y, width, height, color) {}
-function handleInput(player, keys) {}
 function updatePlayer(player, platforms, canvas) {}
-function drawGame(ctx, platforms, player) {}
