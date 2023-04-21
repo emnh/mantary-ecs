4965476837d2b36f5ba88a6934f5f76c9bb56106
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 00:54:54 +0200

    add function_blinkPreTag.js

--- ecs/src/functions/function_blinkPreTag.js
+++ ecs/src/functions/function_blinkPreTag.js
@@ -1,13 +1,5 @@
 function blinkPreTag(preElement, duration) {
   console.log('blinkPreTag');
-  var styleEl = document.getElementById("blink-animation-styles");
-  if (!styleEl) {
-    var styleText = "@keyframes blink { 50% { opacity: 0; } } pre.blink { animation: blink " + duration / 1000 + "s infinite; }";
-    styleEl = document.createElement("style");
-    styleEl.setAttribute("id", "blink-animation-styles");
-    styleEl.appendChild(document.createTextNode(styleText));
-    document.head.appendChild(styleEl);
-  }
   preElement.classList.add("blink");
   setTimeout(function () {
     preElement.classList.remove("blink");
