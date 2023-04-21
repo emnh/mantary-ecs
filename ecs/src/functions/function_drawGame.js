36c4959d48609f43f494519840bc6986eac73e91
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 23:20:30 +0200

    add function_drawGame.js

--- ecs/src/functions/function_drawGame.js
+++ ecs/src/functions/function_drawGame.js
@@ -1,3 +1,15 @@
 function drawGame(ctx, platforms, player) {
-  // drawGame function code here
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw the platforms
+  for (let i = 0; i < platforms.length; i++) {
+    const platform = platforms[i];
+    ctx.fillStyle = platform.color;
+    ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
+  }
+
+  // Draw the player
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.width, player.height);
 }
