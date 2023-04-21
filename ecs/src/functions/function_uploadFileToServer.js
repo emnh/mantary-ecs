b36d58d27b0831712ca97e155ed400ea7f78f122
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 23:49:53 +0200

    add function_uploadFileToServer.js

---
+++ ecs/src/functions/function_uploadFileToServer.js
@@ -0,0 +1,23 @@
+function uploadFileToServer(filename, content) {
+  return new Promise((resolve, reject) => {
+    const url = 'http://localhost:5000/upload';
+    const data = { filename: filename, content: content };
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+    fetch(url, options)
+      .then(response => response.text())
+      .then(result => {
+        console.log(filename, result);
+        resolve(result);
+      })
+      .catch(error => {
+        console.error(filename, error);
+        reject(error);
+      });
+  });
+}
