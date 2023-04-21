45d21e038b2a12c8f6c34c785094bf1702650459
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 00:07:52 +0200

    add function_drawGame.js

--- ecs/src/functions/function_drawGame.js
+++ ecs/src/functions/function_drawGame.js
@@ -1,1 +1,11 @@
-function drawGame(ctx, platforms, player) {}
+function drawGame(ctx, platforms, player) {
+  console.log('drawGame');
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillStyle = '#555555';
+  for (let i = 0; i < platforms.length; i++) {
+    const platform = platforms[i];
+    ctx.fillRect(platform.x, platform.y, platform.width, platform.height);
+  }
+  ctx.fillStyle = '#ff0000';
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+}
