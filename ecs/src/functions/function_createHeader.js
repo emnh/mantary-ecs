769952824a23ca95241c55760c0cda75b3ec9ac5
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 23:20:29 +0200

    add function_createHeader.js

--- ecs/src/functions/function_createHeader.js
+++ ecs/src/functions/function_createHeader.js
@@ -3,6 +3,10 @@ function createHeader(title) {
   header.style.backgroundColor = "black";
   header.style.color = "white";
   header.style.padding = "20px";
-  header.innerHTML = `<h1 style='font-size: 48px;'>${title}</h1>`;
+  const titleDiv = document.createElement("div"); // Create a div element for the title
+  titleDiv.style.fontSize = "48px";
+  titleDiv.style.textAlign = "center"; // Center the text
+  titleDiv.textContent = title; // Set the title text
+  header.appendChild(titleDiv); // Add the title div to the header
   return header;
 }
