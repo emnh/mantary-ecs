ffed2081ddcb830d39de709d3831594ac3eeac8f
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 22:25:49 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -7,19 +7,19 @@ function main() {
   header.style.backgroundColor = "black";
   header.style.color = "white";
   header.style.padding = "20px";
-  header.innerHTML = "<h1>Welcome to my cool webpage!</h1>";
+  header.innerHTML = "<h1 style='font-size: 48px;'>Welcome to my cool webpage!</h1>";
 
   // Create the main content area
   const main = document.createElement("main");
   main.style.padding = "20px";
-  main.innerHTML = "<p>This is the main content area of my cool webpage.</p><p>Here's a cool image:</p><img src='https://picsum.photos/400' alt='A random image from Lorem Picsum'>";
+  main.innerHTML = "<p style='font-size: 24px;'>This is the main content area of my cool webpage.</p><p style='font-size: 24px;'>Here's a cool image:</p><img src='https://picsum.photos/400' alt='A random image from Lorem Picsum'>";
 
   // Create the footer element
   const footer = document.createElement("footer");
   footer.style.backgroundColor = "black";
   footer.style.color = "white";
   footer.style.padding = "20px";
-  footer.innerHTML = "<p>&copy; 2023 My Cool Webpage. All rights reserved.</p>";
+  footer.innerHTML = "<p style='font-size: 24px;'>&copy; 2023 My Cool Webpage. All rights reserved.</p>";
 
   // Add the header, main content area, and footer to the page
   document.body.appendChild(header);
