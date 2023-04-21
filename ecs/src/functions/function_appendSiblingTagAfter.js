edd42e1f48e1ce2298c288a6c35aae949e74047c
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 01:30:20 +0200

    add function_appendSiblingTagAfter.js

--- ecs/src/functions/function_appendSiblingTagAfter.js
+++ ecs/src/functions/function_appendSiblingTagAfter.js
@@ -1,5 +1,6 @@
-function appendSiblingTagAfter(targetElement, newTagName) {
+function appendSiblingTagAfter(targetElement, newTagName, text) {
   console.log('appendSiblingTagAfter');
   const newTag = document.createElement(newTagName);
+  newTag.textContent = text;
   targetElement.insertAdjacentElement("afterend", newTag);
 }
