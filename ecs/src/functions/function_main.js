64311494a3d5106fe03c5daff2109acfb6572117
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 00:28:30 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,6 +1,16 @@
 function main() {
-  loadAssets();
-  setupStates();
-  me.state.change("play");
+  console.log('main');
+  var canvas = document.createElement("canvas");
+  canvas.width = 640;
+  canvas.height = 480;
+  document.body.appendChild(canvas);
+  var game = new melonjs.Game({
+    canvas: canvas,
+    resources: [],
+    states: []
+  });
+  game.onload = function () {
+    me.state.change("play");
+  };
   game.start();
 }
