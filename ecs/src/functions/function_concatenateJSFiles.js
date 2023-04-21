e77207cf288065d294415dccd6ccb828db2cd4d5
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 22:07:33 +0200

    add function_concatenateJSFiles.js

---
+++ ecs/src/functions/function_concatenateJSFiles.js
@@ -0,0 +1,10 @@
+function concatenateJSFiles(dirPath) {
+  const files = fs.readdirSync(dirPath);
+  const jsFiles = files.filter(file => path.extname(file) === '.js');
+  const content = jsFiles.reduce((accumulator, file) => {
+    const filePath = path.join(dirPath, file);
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    return accumulator + fileContent;
+  }, '');
+  fs.writeFileSync(path.join(dirPath, 'main.js'), content);
+}
