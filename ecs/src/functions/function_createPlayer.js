7bdc98a38426e0297c5b5dde3ff68fc6f8f8202b
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 23:20:30 +0200

    add function_createPlayer.js

--- ecs/src/functions/function_createPlayer.js
+++ ecs/src/functions/function_createPlayer.js
@@ -1,14 +1,3 @@
 function createPlayer(x, y, color) {
-  return {
-    x: x,
-    y: y,
-    width: 50,
-    height: 50,
-    speed: 5,
-    color: color,
-    jumping: false,
-    jumpSpeed: 0,
-    jumpHeight: 150,
-    gravity: 0.5
-  };
+  // createPlayer function code here
 }
