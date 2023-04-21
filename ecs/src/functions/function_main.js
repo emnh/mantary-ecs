f83cf7f8f7d27bf0d476298199b176e9609a69cf
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 22:19:23 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,3 +1,20 @@
 function main() {
-  document.body.style.backgroundColor = "red";
+  // Set the background color to blue
+  document.body.style.backgroundColor = "blue";
+
+  // Create a new image element for the jumping donkey
+  const img = document.createElement("img");
+  img.src = "https://i.imgur.com/LrjMQ7V.gif";
+  img.alt = "Jumping Donkey";
+
+  // Set the position and size of the image
+  img.style.position = "absolute";
+  img.style.top = "50%";
+  img.style.left = "50%";
+  img.style.transform = "translate(-50%, -50%)";
+  img.style.width = "200px";
+  img.style.height = "200px";
+
+  // Add the image to the page
+  document.body.appendChild(img);
 }
