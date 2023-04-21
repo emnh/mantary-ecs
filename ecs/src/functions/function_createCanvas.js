be02d9868bb978eeac872ffc96bd850fe9f423e0
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 23:20:44 +0200

    add function_createCanvas.js

--- ecs/src/functions/function_createCanvas.js
+++ ecs/src/functions/function_createCanvas.js
@@ -1,3 +1,8 @@
 function createCanvas(width, height) {
-  // createCanvas function code here
+  const canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  canvas.style.border = "1px solid black";
+  canvas.style.margin = "0 auto";
+  return canvas;
 }
