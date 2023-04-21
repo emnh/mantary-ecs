0b98a8cf539db76daf67a094a2867693f307ede6
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 23:58:55 +0200

    add function_addConsoleLogToFunction.js

--- ecs/src/functions/function_addConsoleLogToFunction.js
+++ ecs/src/functions/function_addConsoleLogToFunction.js
@@ -1,12 +1,13 @@
+const acorn = require('acorn');
 function addConsoleLogToFunction(parsed) {
-  // Find the FunctionDeclaration node
+  console.log('addConsoleLogToFunction');
   const functionNode = parsed.body.find(node => node.type === 'FunctionDeclaration');
-
-  // Add a console.log statement that outputs the function name
   const functionName = functionNode.id.name;
   const consoleLogNode = acorn.parse(`console.log('${functionName}');`).body[0];
   functionNode.body.body.unshift(consoleLogNode);
-
-  // Return the modified parsed object
   return parsed;
 }
+const code = 'function hello() { console.log("Hello, world!"); }';
+const parsed = acorn.parse(code);
+const modified = addConsoleLogToFunction(parsed);
+console.log(modified.toString());
