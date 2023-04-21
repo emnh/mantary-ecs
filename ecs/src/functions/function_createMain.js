6ae76fd9144f2d8c49e65cec685c357a2f98b34e
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-21 22:42:52 +0200

    add function_createMain.js

--- ecs/src/functions/function_createMain.js
+++ ecs/src/functions/function_createMain.js
@@ -2,9 +2,6 @@ function createMain(content) {
   const main = document.createElement("main");
   main.style.padding = "20px";
   main.style.paddingBottom = "40px";
-  main.innerHTML = `
-    <p style='font-size: 24px;'>${content}</p>
-    <div style='width: 400px; height: 400px; background-color: black;'></div>
-  `;
+  main.innerHTML = `<p style='font-size: 24px;'>${content}</p>`;
   return main;
 }
