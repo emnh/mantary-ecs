04b730d5e8c632cb4c2a4f3274f0d091145be4b4
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 00:44:57 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,19 +1,20 @@
 function main() {
   console.log('main');
-  var canvas = document.createElement("canvas");
-  canvas.width = 640;
-  canvas.height = 480;
-  document.body.appendChild(canvas);
-  me.boot();
-  me.video.init(canvas.width, canvas.height, {
-    wrapper: canvas,
-    renderer: me.video.CANVAS,
-    scale: "auto",
-    scaleMethod: "fit",
-    doubleBuffering: true
+  me.initialize();
+  me.loader.load({}, function () {
+    me.state.change(me.state.PLAY);
   });
-  me.loader.preload([], function onLoad() {
-    me.state.change("play");
-    me.game.repaint();
+  me.state.PLAY = me.state.extend({
+    onResetEvent: function () {
+      var player = new Player(0, 0);
+      me.game.world.addChild(player);
+      var platform1 = new Platform(0, 400);
+      me.game.world.addChild(platform1);
+      var platform2 = new Platform(200, 300);
+      me.game.world.addChild(platform2);
+      var platform3 = new Platform(400, 200);
+      me.game.world.addChild(platform3);
+    }
   });
+  me.start();
 }
