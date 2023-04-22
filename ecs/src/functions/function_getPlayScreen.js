a4b10b658f54a8666065597e6f3c94aaa082f280
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 08:12:49 +0200

    add function_getPlayScreen.js

--- ecs/src/functions/function_getPlayScreen.js
+++ ecs/src/functions/function_getPlayScreen.js
@@ -5,7 +5,14 @@ function getPlayScreen() {
       super();
     }
     onResetEvent() {
-      const platformImage = me.loader.getImage('../images/platform2.png');
+      let platformImage = me.loader.getImage('../images/platform2.png');
+      if (!platformImage) {
+        platformImage = me.loader.getImage('/images/platform2.png');
+      }
+      if (!platformImage) {
+        console.error("Failed to load platform image!");
+        return;
+      }
       drawPlatform(me.game, 100, 400, 200, 50, platformImage);
     }
     onDestroyEvent() {}
