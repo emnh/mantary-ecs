76696ca09dc73c1e110de3139d37d44b6f9d70b2
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 08:41:53 +0200

    add function_getPlayScreen.js

--- ecs/src/functions/function_getPlayScreen.js
+++ ecs/src/functions/function_getPlayScreen.js
@@ -1,21 +1,23 @@
 function getPlayScreen(images) {
   console.log('getPlayScreen');
+  const PlayerEntity = getPlayerEntity();
   class PlayScreen extends me.Stage {
     constructor() {
       super();
       this.backgroundColor = '#202020';
       drawPlatform(me.game.world, 0, 500, 800, 100, images.platform);
-      drawPlayer(me.game.world, 0, 0, 64, 64, images.player);
+      const player = new PlayerEntity(0, 0, {
+        image: images.player
+      });
+      player.renderable.scale(2, 2);
+      player.body.gravityScale = 0;
+      player.body.collisionType = me.collision.types.PLAYER_OBJECT;
+      player.body.setVelocity(3, 3);
+      player.body.setMaxVelocity(3, 3);
+      me.game.world.addChild(player);
     }
     onResetEvent() {
       const {PlayScreen} = me.state;
-      PlayScreen.player = new me.PlayerEntity(0, 0, {});
-      PlayScreen.player.renderable.scale(2, 2);
-      PlayScreen.player.body.gravityScale = 0;
-      PlayScreen.player.body.collisionType = me.collision.types.PLAYER_OBJECT;
-      PlayScreen.player.body.setVelocity(3, 3);
-      PlayScreen.player.body.setMaxVelocity(3, 3);
-      drawPlatform(me.game.world, 0, 500, 800, 100, images.platform);
       me.input.bindKey(me.input.KEY.LEFT, "left");
       me.input.bindKey(me.input.KEY.RIGHT, "right");
       me.input.bindKey(me.input.KEY.UP, "jump", true);
