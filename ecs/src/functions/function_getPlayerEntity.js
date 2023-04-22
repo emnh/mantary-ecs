0030baab3c1d39e17e2b730ff01559ffa17040ee
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 07:11:35 +0200

    add function_getPlayerEntity.js

--- ecs/src/functions/function_getPlayerEntity.js
+++ ecs/src/functions/function_getPlayerEntity.js
@@ -2,33 +2,26 @@ function getPlayerEntity() {
   console.log('getPlayerEntity');
   class PlayerEntity extends me.Entity {
     constructor(x, y, settings) {
-      super(x, y, Object.assign(settings, {
-        width: 32,
-        height: 32
-      }));
-      this.body = new me.Body(this, new me.Rect(0, 0, 32, 32));
+      super(x, y, settings);
       this.body.setVelocity(3, 3);
-      this.body.collisionType = me.collision.types.PLAYER_OBJECT;
-      this.renderable = new me.ColorLayer("player", "#00ff00", 32, 32);
-      this.anchorPoint.set(0.5, 0.5);
+      this.body.setMaxVelocity(6, 6);
+      me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+      this.alwaysUpdate = true;
+      const gfx = new me.GraphicsRenderer();
+      gfx.createRenderable(0, 0, this.width, this.height);
+      gfx.fillStyle = "#00FF00";
+      gfx.fillRect(0, 0, this.width, this.height);
+      this.renderable = gfx;
+      setupKeyboardInput.call(this);
     }
     update(dt) {
-      super.update(dt);
-      if (me.input.isKeyPressed("left") || me.input.isKeyPressed("a")) {
-        this.body.vel.x -= this.body.accel.x * me.timer.tick;
-      }
-      if (me.input.isKeyPressed("right") || me.input.isKeyPressed("d")) {
-        this.body.vel.x += this.body.accel.x * me.timer.tick;
-      }
-      if (me.input.isKeyPressed("up") || me.input.isKeyPressed("w")) {
-        this.body.vel.y -= this.body.accel.y * me.timer.tick;
-      }
-      if (me.input.isKeyPressed("down") || me.input.isKeyPressed("s")) {
-        this.body.vel.y += this.body.accel.y * me.timer.tick;
-      }
       this.body.update(dt);
       me.collision.check(this);
-      return true;
+      if (this.body.vel.x !== 0 || this.body.vel.y !== 0) {
+        this._super(me.Entity, "update", [dt]);
+        return true;
+      }
+      return false;
     }
   }
   return PlayerEntity;
