aec0046351dc118a6db5e6c7a4813fa4667be998
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 23:29:03 +0200

    add function_playerUpdate.js

---
+++ ecs/src/functions/function_playerUpdate.js
@@ -0,0 +1,20 @@
+function playerUpdate(thisPlayer, superUpdate, dt) {
+  console.log('playerUpdate');
+  handleMoveMentLeftRight(thisPlayer);
+  handleJump(thisPlayer);
+  if (thisPlayer.body.force.x === 0 && thisPlayer.body.force.y === 0) {}
+  if (!thisPlayer.inViewport && thisPlayer.pos.y > me.video.renderer.getHeight()) {
+    me.game.world.removeChild(thisPlayer);
+    me.game.viewport.fadeIn("#fff", 150, function () {
+      me.audio.play("die", false);
+      me.level.reload();
+      me.game.viewport.fadeOut("#fff", 150);
+    });
+    return true;
+  }
+  if (thisPlayer.body.vel.x !== 0 || thisPlayer.body.vel.y !== 0 || thisPlayer.renderable && thisPlayer.renderable.isFlickering()) {
+    superUpdate(dt);
+    return true;
+  }
+  return false;
+}
