0cecc770f869796f913078e7b7df5fc75318fb0b
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 07:40:09 +0200

    add function_loadMelonJS.js

--- ecs/src/functions/function_loadMelonJS.js
+++ ecs/src/functions/function_loadMelonJS.js
@@ -1,6 +1,15 @@
 function loadMelonJS() {
   console.log('loadMelonJS');
-  const script = document.createElement('script');
-  script.src = 'https://esm.run/melonjs';
-  document.head.appendChild(script);
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = 'https://esm.run/melonjs';
+    script.addEventListener('load', () => {
+      resolve();
+    });
+    script.addEventListener('error', () => {
+      reject(new Error('Failed to load melonJS'));
+    });
+    document.head.appendChild(script);
+  });
 }
