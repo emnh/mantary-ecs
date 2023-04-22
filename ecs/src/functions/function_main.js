9a551d05144c79f9520078164fb8bf361038f7ae
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 06:28:10 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,5 +1,6 @@
 async function main() {
   console.log('main');
   const melonjs = await import("https://esm.run/melonjs@latest/dist/melonjs.module.js");
+  console.log("melonjs loaded:", melonjs.default);
   example(melonjs.default);
 }
