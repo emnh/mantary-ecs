cfcc4609364c4437ce1eb6c856271e2ae278c352
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 06:32:41 +0200

    add function_setupKeyboardInput.js

---
+++ ecs/src/functions/function_setupKeyboardInput.js
@@ -0,0 +1,6 @@
+function setupKeyboardInput() {
+  me.input.bindKey(me.input.KEY.LEFT, "left");
+  me.input.bindKey(me.input.KEY.RIGHT, "right");
+  me.input.bindKey(me.input.KEY.UP, "up");
+  me.input.bindKey(me.input.KEY.DOWN, "down");
+}
