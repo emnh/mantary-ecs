647a28012512c58fa0980fa9fb2595afd5973311
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 06:10:26 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,22 +1,9 @@
 function main() {
-  console.log('main');
-  const me = window.me;
-  me.device.onReady(function () {
-    if (!me.video.init(1218, 562, {
-      parent: "screen",
-      scale: "auto"
-    })) {
-      alert("Your browser does not support HTML5 canvas.");
-      return;
-    }
-    me.game.world.addChild(new me.ColorLayer("background", "#202020"));
-    me.game.world.addChild(new me.Text(609, 281, {
-      font: "Arial",
-      size: 160,
-      fillStyle: "#FFFFFF",
-      textBaseline: "middle",
-      textAlign: "center",
-      text: "Hello World !"
-    }));
-  });
+  const script = document.createElement("script");
+  script.src = "https://esm.run/melonjs";
+  script.async = true;
+  script.onload = function () {
+    example();
+  };
+  document.head.appendChild(script);
 }
