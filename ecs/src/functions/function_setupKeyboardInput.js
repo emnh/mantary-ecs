8ef2cb992651a890b7083d728cb357792f1112bf
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 07:12:42 +0200

    add function_setupKeyboardInput.js

--- ecs/src/functions/function_setupKeyboardInput.js
+++ ecs/src/functions/function_setupKeyboardInput.js
@@ -1,4 +1,4 @@
-function setupKeyboardInput() {
+function setupKeyboardInput(me, player) {
   me.input.bindKey(me.input.KEY.LEFT, "left");
   me.input.bindKey(me.input.KEY.RIGHT, "right");
   me.input.bindKey(me.input.KEY.UP, "up");
@@ -7,9 +7,4 @@ function setupKeyboardInput() {
   me.input.bindKey(me.input.KEY.A, "a");
   me.input.bindKey(me.input.KEY.S, "s");
   me.input.bindKey(me.input.KEY.D, "d");
-  me.input.bindKey(me.input.KEY.SPACE, "space");
-  me.input.bindPointer(me.input.KEY.LEFT);
-  me.input.bindPointer(me.input.KEY.RIGHT);
-  me.input.bindPointer(me.input.KEY.UP);
-  me.input.bindPointer(me.input.KEY.DOWN);
 }
