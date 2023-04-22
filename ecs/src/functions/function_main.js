0054cf7790a9971336168cafb40749eebc59d562
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 07:41:29 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,10 +1,3 @@
-async function main() {
-  console.log('main');
-  try {
-    await loadMelonJS();
-    console.log('melonJS has been loaded!');
-    const me = window.me;
-  } catch (error) {
-    console.error(error);
-  }
+function main() {
+  loadDependencies();
 }
