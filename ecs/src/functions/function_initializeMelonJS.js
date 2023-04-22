c0f62cdcdced46c7295057518ff86ab4ee8ba37a
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 08:33:45 +0200

    add function_initializeMelonJS.js

--- ecs/src/functions/function_initializeMelonJS.js
+++ ecs/src/functions/function_initializeMelonJS.js
@@ -1,4 +1,5 @@
 async function initializeMelonJS() {
+  console.log('initializeMelonJS');
   const melonScript = await loadMelonJS();
   me.video.init(window.innerWidth, window.innerHeight, {
     wrapper: "screen",
@@ -6,7 +7,7 @@ async function initializeMelonJS() {
     renderer: me.video.CANVAS
   });
   me.audio.init("mp3,ogg");
-  const imageFilenames = ['platform2.png', 'walk.gif'];
+  const imageFilenames = ['platform2.png', 'protagonist.png'];
   const images = await loadImages(imageFilenames);
   me.state.set(me.state.PLAY, new (await getPlayScreen(images))());
   me.state.change(me.state.PLAY);
