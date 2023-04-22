893d9ad8c68e3f2594f5922c8f9fcc008cc6089b
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 08:34:47 +0200

    add function_loadImages.js

--- ecs/src/functions/function_loadImages.js
+++ ecs/src/functions/function_loadImages.js
@@ -1,14 +1,14 @@
-async function loadImages(imageFilenames) {
+async function loadImages(imageDict) {
   console.log('loadImages');
   const images = {};
-  for (const filename of imageFilenames) {
-    const imageUrl = `./images/${filename}`;
+  for (const [name, url] of Object.entries(imageDict)) {
+    const imageUrl = `./images/${url}`;
     const image = await getImage(imageUrl);
     if (!image) {
       console.error(`Failed to load image from URL: ${imageUrl}`);
       continue;
     }
-    images[filename] = image;
+    images[name] = image;
   }
   return images;
 }
