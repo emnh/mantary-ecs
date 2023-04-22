5a96bb66411bced7f72af63ac697ab8c6c0f4e01
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 23:26:32 +0200

    add function_getAddPlatform.js

---
+++ ecs/src/functions/function_getAddPlatform.js
@@ -0,0 +1,23 @@
+function getAddPlatform(makeAnyEntity, world, imageURL) {
+  console.log('getAddPlatform');
+  const platformImg = new Image();
+  platformImg.src = imageURL;
+  const s = 0.4;
+  const width = Math.round(675 * s);
+  const height = Math.round(618 * s);
+  platformImg.width = width;
+  platformImg.height = height;
+  return function (x, y) {
+    world.addChild(makeAnyEntity(x, y, {
+      image: platformImg,
+      width: width,
+      height: height,
+      scale: 1,
+      ignoreGravity: true,
+      myCollisionType: me.collision.types.WORLD_SHAPE,
+      onCollision: (response, other) => {
+        return false;
+      }
+    }));
+  };
+}
