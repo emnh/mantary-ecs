54135ca2c9c0ca38eb3c5ece614edc3dec94a45f
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 07:43:44 +0200

    add function_drawRectangle.js

---
+++ ecs/src/functions/function_drawRectangle.js
@@ -0,0 +1,8 @@
+function drawRectangle(x, y, width, height, color) {
+  const rect = new me.Renderable(x, y, width, height);
+  rect.draw = function (renderer) {
+    renderer.setColor(color);
+    renderer.fillRect(this.pos.x, this.pos.y, this.width, this.height);
+  };
+  game.world.addChild(rect);
+}
