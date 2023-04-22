40c1ac094333af32f99a6d98ba56e3283f51d509
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 06:20:58 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,8 +1,9 @@
 function main() {
+  console.log('main');
   const script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/npm/melonjs@15.1.1/dist/melonjs.min.js";
+  script.src = "https://cdn.jsdelivr.net/npm/melonjs@15.1.1/dist/melonjs.module.js";
   script.async = false;
-  script.type = "text/javascript";
+  script.type = "module";
   script.onload = function () {
     example();
   };
