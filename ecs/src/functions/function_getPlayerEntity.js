90097c85b431a9fe0b01c450340acd4ef42c1b0a
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 06:44:53 +0200

    add function_getPlayerEntity.js

--- ecs/src/functions/function_getPlayerEntity.js
+++ ecs/src/functions/function_getPlayerEntity.js
@@ -3,13 +3,7 @@ function getPlayerEntity(me) {
   class PlayerEntity extends me.Entity {
     constructor(x, y, settings) {
       super(x, y, settings);
-      this.speed = 5;
-      settings.image.addEventListener("loaded", () => {
-        this.renderable.addAnimation("stand", [0]);
-        this.renderable.addAnimation("walk", [1, 2, 3, 4, 5, 6, 7, 8], 80);
-        this.renderable.setCurrentAnimation("stand");
-      });
-      this.body.setShape(new me.Rect(0, 0, 32, 64));
+      console.log("settings:", settings);
     }
   }
   return PlayerEntity;
