67f212f448e96eb2d287dd1e09567041d21ff971
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 06:52:23 +0200

    add function_getPlayerEntity.js

--- ecs/src/functions/function_getPlayerEntity.js
+++ ecs/src/functions/function_getPlayerEntity.js
@@ -1,10 +1,1 @@
-function getPlayerEntity(me) {
-  console.log('getPlayerEntity');
-  class PlayerEntity extends me.Entity {
-    constructor(x, y, settings) {
-      super(x, y, settings);
-      console.log("settings:", settings);
-    }
-  }
-  return PlayerEntity;
-}
+function getPlayerEntity(me) {}
