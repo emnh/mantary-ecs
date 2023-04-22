432a61923ec8eae49bafd1ec97e9838d399e8293
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 07:47:19 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,5 +1,11 @@
-function main() {
-  initializeMelonJS(game => {
-    drawRectangle(game, 100, 100, 50, 50, '#FF0000');
-  });
+async function main() {
+  try {
+    await loadDependencies();
+    console.log('Dependencies have been loaded!');
+    initializeMelonJS(game => {
+      drawRectangle(game, 100, 100, 50, 50, '#FF0000');
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
