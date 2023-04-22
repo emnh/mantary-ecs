898dc291c3f2f10a54bd342a980d724c6f644f9c
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 02:21:32 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,4 +1,15 @@
 function main() {
   console.log('main');
-  document.body.style.backgroundColor = "blue";
+  const rows = 5;
+  const cols = 5;
+  const table = document.createElement("table");
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement("tr");
+    for (let j = 0; j < cols; j++) {
+      const cell = document.createElement("td");
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+  }
+  document.body.appendChild(table);
 }
