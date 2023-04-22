b7e5c11e647db8510921d7e117b807f96fd56db1
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 07:10:59 +0200

    add function_getPlayerEntity.js

--- ecs/src/functions/function_getPlayerEntity.js
+++ ecs/src/functions/function_getPlayerEntity.js
@@ -1,35 +1,35 @@
-function getPlayerEntity(x, y, settings) {
+function getPlayerEntity() {
   console.log('getPlayerEntity');
   class PlayerEntity extends me.Entity {
     constructor(x, y, settings) {
-      settings = settings || ({});
-      settings.width = settings.width || 32;
-      settings.height = settings.height || 64;
-      super(x, y, settings);
-      this.speed = 5;
-      this.body = new me.Body(this);
-      this.body.addShape(new me.Rect(0, 0, settings.width, settings.height), true);
+      super(x, y, Object.assign(settings, {
+        width: 32,
+        height: 32
+      }));
+      this.body = new me.Body(this, new me.Rect(0, 0, 32, 32));
+      this.body.setVelocity(3, 3);
       this.body.collisionType = me.collision.types.PLAYER_OBJECT;
-      const renderable = new me.Renderable(0, 0, settings.width, settings.height);
-      renderable.anchorPoint.set(0.5, 0.5);
-      renderable.color = new me.Color(0, 255, 0);
-      this.body.setShape(renderable);
+      this.renderable = new me.ColorLayer("player", "#00ff00", 32, 32);
+      this.anchorPoint.set(0.5, 0.5);
     }
     update(dt) {
       super.update(dt);
-      if (me.input.isKeyPressed("left") || me.input.isKeyPressed("A")) {
-        this.body.vel.x -= this.speed * me.timer.tick;
-      } else if (me.input.isKeyPressed("right") || me.input.isKeyPressed("D")) {
-        this.body.vel.x += this.speed * me.timer.tick;
+      if (me.input.isKeyPressed("left") || me.input.isKeyPressed("a")) {
+        this.body.vel.x -= this.body.accel.x * me.timer.tick;
       }
-      if (me.input.isKeyPressed("up") || me.input.isKeyPressed("W")) {
-        this.body.vel.y -= this.speed * me.timer.tick;
-      } else if (me.input.isKeyPressed("down") || me.input.isKeyPressed("S")) {
-        this.body.vel.y += this.speed * me.timer.tick;
+      if (me.input.isKeyPressed("right") || me.input.isKeyPressed("d")) {
+        this.body.vel.x += this.body.accel.x * me.timer.tick;
+      }
+      if (me.input.isKeyPressed("up") || me.input.isKeyPressed("w")) {
+        this.body.vel.y -= this.body.accel.y * me.timer.tick;
+      }
+      if (me.input.isKeyPressed("down") || me.input.isKeyPressed("s")) {
+        this.body.vel.y += this.body.accel.y * me.timer.tick;
       }
       this.body.update(dt);
+      me.collision.check(this);
       return true;
     }
   }
-  return new PlayerEntity(x, y, settings);
+  return PlayerEntity;
 }
