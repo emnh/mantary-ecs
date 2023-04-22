a8e921e9f4d73fbb80ccb7f007290cd710903c4a
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 07:02:50 +0200

    add function_getPlayerEntity.js

--- ecs/src/functions/function_getPlayerEntity.js
+++ ecs/src/functions/function_getPlayerEntity.js
@@ -7,8 +7,9 @@ function getPlayerEntity(me) {
       settings.height = settings.height || 64;
       super(x, y, settings);
       this.renderable = new me.Renderable(0, 0, settings.width, settings.height);
-      this.renderable.anchorPoint.set(0, 0);
+      this.renderable.anchorPoint.set(0.5, 0.5);
       this.renderable.color = new me.Color(0, 255, 0);
+      this.addChild(this.renderable);
       this.speed = 5;
       this.body = new me.Body(this);
       this.body.addShape(new me.Rect(0, 0, settings.width, settings.height));
