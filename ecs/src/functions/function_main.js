a87942bb4a05a82c5334db156ed3b7efd9467f37
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 02:19:57 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,6 +1,4 @@
 function main() {
   console.log('main');
-  const h1 = document.createElement("h1");
-  h1.textContent = "Goodbye, World!";
-  document.body.appendChild(h1);
+  document.body.style.backgroundColor = "red";
 }
