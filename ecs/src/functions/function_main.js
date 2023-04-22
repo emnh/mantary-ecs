293a451f784220100d5729a67088cdf6297f1c83
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 06:08:07 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,3 +1,22 @@
 function main() {
-  redirectToGoogle();
+  console.log('main');
+  const me = window.me;
+  me.device.onReady(function () {
+    if (!me.video.init(1218, 562, {
+      parent: "screen",
+      scale: "auto"
+    })) {
+      alert("Your browser does not support HTML5 canvas.");
+      return;
+    }
+    me.game.world.addChild(new me.ColorLayer("background", "#202020"));
+    me.game.world.addChild(new me.Text(609, 281, {
+      font: "Arial",
+      size: 160,
+      fillStyle: "#FFFFFF",
+      textBaseline: "middle",
+      textAlign: "center",
+      text: "Hello World !"
+    }));
+  });
 }
