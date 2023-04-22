229c97ec4ce5f42199b75b9ae7537da2a1adbe6c
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 08:19:26 +0200

    add function_getPlayScreen.js

--- ecs/src/functions/function_getPlayScreen.js
+++ ecs/src/functions/function_getPlayScreen.js
@@ -1,10 +1,11 @@
 async function getPlayScreen() {
+  console.log('getPlayScreen');
   class PlayScreen extends me.Stage {
     constructor() {
       super();
     }
     async onResetEvent() {
-      const platformImage = await getImage('/images/platform2');
+      const platformImage = await getImage('/images/platform2.png');
       if (!platformImage) {
         console.error("Failed to load platform image!");
         return;
