7e7fd5cb8e43b09591ca465905ab5d0abf84be58
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 08:49:55 +0200

    add function_getPlayerEntity.js

--- ecs/src/functions/function_getPlayerEntity.js
+++ ecs/src/functions/function_getPlayerEntity.js
@@ -2,17 +2,41 @@ function getPlayerEntity() {
   console.log('getPlayerEntity');
   class PlayerEntity extends me.Entity {
     constructor(x, y, settings) {
-      super(x, y, {
-        width: settings.width,
-        height: settings.height
-      });
-      this.renderable = new me.Sprite(0, 0, {
-        image: settings.image,
-        framewidth: 64,
-        frameheight: 64
-      });
+      super(x, y, settings);
+      this.alwaysUpdate = true;
       this.renderable.addAnimation("walk", [0, 1, 2, 3, 4, 5, 6, 7]);
       this.renderable.setCurrentAnimation("walk");
+      this.body.setMaxVelocity(3, 15);
+      me.game.viewport.follow(this, me.game.viewport.AXIS.BOTH, 0.1);
+    }
+    update(dt) {
+      if (me.input.isKeyPressed("left")) {
+        this.body.vel.x -= this.body.accel.x * me.timer.tick;
+        this.renderable.flipX(true);
+      } else if (me.input.isKeyPressed("right")) {
+        this.body.vel.x += this.body.accel.x * me.timer.tick;
+        this.renderable.flipX(false);
+      } else {
+        this.body.vel.x = 0;
+      }
+      if (me.input.isKeyPressed("jump")) {
+        if (!this.body.jumping && !this.body.falling) {
+          this.body.vel.y = -this.body.maxVel.y * me.timer.tick;
+          this.body.jumping = true;
+        }
+      }
+      this.body.update(dt);
+      me.collision.check(this);
+      return super.update(dt);
+    }
+    onCollision(response, other) {
+      if (other.body.collisionType === me.collision.types.WORLD_SHAPE) {
+        if (response.b.y > 0) {
+          this.body.falling = false;
+          this.body.vel.y = -this.body.maxVel.y * me.timer.tick;
+          this.body.jumping = true;
+        }
+      }
     }
   }
   return PlayerEntity;
