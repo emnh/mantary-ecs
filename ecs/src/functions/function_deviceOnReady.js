cf6b119b271954314d94d367a4e5d6067fcc9cc8
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 23:31:12 +0200

    add function_deviceOnReady.js

---
+++ ecs/src/functions/function_deviceOnReady.js
@@ -0,0 +1,17 @@
+function deviceOnReady() {
+  console.log('deviceOnReady');
+  if (!me.video.init(3840, 2160, {
+    parent: "app",
+    scaleMethod: "flex-width",
+    renderer: me.video.AUTO,
+    preferWebGL1: false,
+    subPixel: true,
+    antiAlias: true
+  })) {
+    alert("Your browser does not support HTML5 canvas.");
+    return;
+  }
+  const makeAnyEntity = getAnyEntity();
+  addLevel(makeAnyEntity, me.game.world);
+  addPlayer(makeAnyEntity, me.game.world);
+}
