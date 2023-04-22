34d07efb363d1e3a2038ecf298abb1a1697ae9ac
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 07:12:41 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -1,18 +1,14 @@
 async function main() {
-  console.log('main');
-  const me = await import("https://cdn.jsdelivr.net/npm/melonjs@latest/dist/melonjs.module.js");
-  me.device.onReady(() => {
-    if (!me.video.init(1218, 562, {
-      parent: "screen",
-      scale: "auto"
-    })) {
-      alert("Your browser does not support HTML5 canvas.");
-      return;
-    }
-    me.game.world.addChild(new me.ColorLayer("background", "#202020"));
-    const PlayerEntity = getPlayerEntity(me);
-    const player = new PlayerEntity(0, 0);
-    me.game.world.addChild(player);
-    setupKeyboardInput(me, player);
+  const me = await import("https://esm.run/melonjs");
+  me.boot();
+  me.video.init(1218, 562, {
+    parent: "screen",
+    scale: "auto",
+    renderer: me.video.RENDERER_WEBGL
   });
+  const PlayerEntity = getPlayerEntity(me);
+  const player = new PlayerEntity(100, 100);
+  setupKeyboardInput(me, player);
+  me.game.world.addChild(player);
+  me.state.change(me.state.PLAY);
 }
