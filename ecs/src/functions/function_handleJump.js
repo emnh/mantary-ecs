baa86d3cacd5cfbd297d5e1132bcff978b882897
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 23:29:00 +0200

    add function_handleJump.js

---
+++ ecs/src/functions/function_handleJump.js
@@ -0,0 +1,15 @@
+function handleJump(thisPlayer) {
+  console.log('handleJump');
+  if (me.input.isKeyPressed("jump")) {
+    thisPlayer.body.jumping = true;
+    if (thisPlayer.multipleJump <= 2) {
+      thisPlayer.body.force.y = -thisPlayer.body.maxVel.y * thisPlayer.multipleJump++;
+    }
+  } else {
+    if (!thisPlayer.body.falling && !thisPlayer.body.jumping) {
+      thisPlayer.multipleJump = 1;
+    } else if (thisPlayer.body.falling && thisPlayer.multipleJump < 2) {
+      thisPlayer.multipleJump = 2;
+    }
+  }
+}
