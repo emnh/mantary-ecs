9435bfad78147486e8346cba8b7b167065acc936
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2023-04-22 02:22:57 +0200

    add function_main.js

--- ecs/src/functions/function_main.js
+++ ecs/src/functions/function_main.js
@@ -4,10 +4,19 @@ function main() {
   function render() {
     document.body.innerHTML = "";
     const table = document.createElement("table");
+    table.style.borderCollapse = "collapse";
     for (let i = 0; i < board.length; i++) {
       const row = document.createElement("tr");
       for (let j = 0; j < board[i].length; j++) {
         const cell = document.createElement("td");
+        cell.style.width = "50px";
+        cell.style.height = "50px";
+        cell.style.textAlign = "center";
+        cell.style.verticalAlign = "middle";
+        cell.style.border = "1px solid black";
+        cell.style.cursor = "pointer";
+        cell.style.fontSize = "36px";
+        cell.style.fontWeight = "bold";
         cell.textContent = board[i][j];
         cell.addEventListener("click", () => {
           if (board[i][j] === "") {
